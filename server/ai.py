7e23342a0622922d1b0acc0e9294d341a58d352d
Paul Adams <paul@thecoderszone.com>
2026-01-31 16:17:50 +0000

    Changes

--- server/ai.py
+++ server/ai.py
@@ -2,16 +2,16 @@ from google import genai
 import time
 import json
 
-with open('mockEmails.json') as f:
-    d = str(json.load(f))
 
 import os
+from dotenv import load_dotenv
 
 
 
 
 ### currently designed as one email at a time
-def is_newsletter(email_input):
+def reading_email(email_input) -> bool:
+    load_dotenv()  # reads .env
     #hidden api
     api_key = os.getenv("google_api")
     client = genai.Client(api_key=api_key)
@@ -19,13 +19,17 @@ def is_newsletter(email_input):
     start_time = time.time()
     ##generating response
     response = client.models.generate_content(
-        model="gemini-2.5-flash", contents= "read only the from,subject and snippet field and check if its a spam/newsletter and only return true or false with a comma for separation and nothing else." + email_input)  ##example id
+        model="gemini-2.5-flash", contents= "read only the from,subject and snippet field and check if its a spam/newsletter and only return true or false and nothing else." + email_input)  ##example id
     
 
     #filtering reponse to remove useless html parts
     text = response.candidates[0].content.parts[0].text
-    print(text)
-    
     end = time.time()
 
-    print("this took ", end - start_time)
+    print(f"Got response {text} for email {email_input['subject']} in time {end - start_time}")
+    return text == "true"
+
+    
+
+##calling function
+reading_email(d)
