4d109fb9511e4da6f0662f65d2839e7e42e96531
Kajuran <znac167@live.rhul.ac.uk>
2026-01-31 14:26:54 +0000

    Made mock_emails() in server.py to load from mockEmails.json

--- server/server.py
+++ server/server.py
@@ -89,11 +89,7 @@ def get_auth_url():
 
 @app.get("/api/mock-emails")
 def mock_emails():
-    return [
-        {
-            "id": "1",
-            "sender": "Medium",
-            "subject": "Your weekly digest",
-            "unsubscribeUrl": "https://medium.com/unsub"
-        }
-    ]
+    with open('../mockEmails.json') as f:
+        mockEmails = json.load(f)
+
+    return mockEmails
