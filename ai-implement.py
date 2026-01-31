41b71d1096f152f082549c3397667cd596173745
jamal <znac510>
2026-01-31 13:06:50 +0000

    #6 adding comments to describe code

--- ai-implement.py
+++ ai-implement.py
@@ -10,16 +10,20 @@ from dotenv import load_dotenv
 
 load_dotenv()  # reads .env
 
+#hidden api
 api_key = os.getenv("google_api")
 
 
+### currently designed as one email at a time
 def reading_email(email_input):
     client = genai.Client(api_key=api_key)
-
+    ##starting timer
     start_time = time.time()
+    ##generating response
     response = client.models.generate_content(
-        model="gemini-2.5-flash", contents= "read this email and rank it where its spam or a newsletter reply with just yes or no and reply with the email sent with" + email_input)  ##example id
+        model="gemini-2.5-flash", contents= "read this email and rank it where its spam or a newsletter reply with just (yes this is a newsletter) or (no it is not) and reply with the email sent with" + email_input)  ##example id
 
+    #filtering reponse to remove useless html parts
     text = response.candidates[0].content.parts[0].text
     print(text)
     end = time.time()
@@ -28,5 +32,5 @@ def reading_email(email_input):
 
     
 
-
+##calling function
 reading_email(d)
