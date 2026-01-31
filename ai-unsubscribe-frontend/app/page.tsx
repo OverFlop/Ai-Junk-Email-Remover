524da5a9b004893d61124da4264940e70cee27a1
Kajuran <znac167@live.rhul.ac.uk>
2026-01-31 15:08:13 +0000

    Trying to enable google auth in front end

--- ai-unsubscribe-frontend/app/page.tsx
+++ ai-unsubscribe-frontend/app/page.tsx
@@ -1,32 +1,19 @@
 "use client";
+import { signIn } from "next-auth/react";
 
 export default function Home() {
-    async function handleScan() {
-        /*const res = await fetch("http://127.0.0.1:5000/api/authurl");
-        const data = await res.json();
-        window.location.href = data.url;*/
-        const res = await fetch("http://127.0.0.1:5000/api/mock-emails");//For testing
-        const data = await res.json();
-        console.log("Mock emails:", data);
-
-    }
-
     return (
         <main className="flex min-h-screen items-center justify-center bg-zinc-50">
             <div className="w-full max-w-md rounded-xl bg-white p-8 shadow">
-                <h1 className="mb-4 text-2xl font-semibold text-zinc-900">
-                    AI Email Unsubscribe
-                </h1>
-
+                <h1 className="mb-4 text-2xl font-semibold">AI Email Unsubscribe</h1>
                 <p className="mb-6 text-zinc-600">
                     Scan your inbox and unsubscribe from unwanted newsletters.
                 </p>
-
                 <button
-                    onClick={handleScan}
-                    className="w-full rounded-lg bg-black py-3 text-white hover:bg-zinc-800"
+                    onClick={() => signIn("google")}
+                    className="w-full rounded-lg bg-black py-3 text-white"
                 >
-                    Scan Inbox
+                    Sign in with Google
                 </button>
             </div>
         </main>
